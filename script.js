007d42f44f259e1ae7db060dfca7d9e20d837fbe
agrawalrohit937 <158179406+agrawalrohit937@users.noreply.github.com>
2026-01-05 08:03:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -156,3 +156,14 @@ if (!isMobile) {
   });
 
 }
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (scrollTop / scrollHeight) * 100;
+  
+  const progressBar = document.querySelector(".scroll-progress");
+  if(progressBar) {
+    progressBar.style.width = scrolled + "%";
+  }
+});
